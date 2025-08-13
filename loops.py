0b4984f26da63a2f5b4ede055cdeaf10742e5427
Arjun Sudhakar Shahapure <195315873+ArjunSudhakarShahapure@users.noreply.github.com>
2025-08-13 16:13:48 +0530

    loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+# # x= ['arjun', 34 ,112, 2102340 ,'2311E8OU']
+# # for I in x:
+# #   print(i)
+
+
+# a=['amruta','arjun','navin reddy']
+# b={"arjun",23,"rit"}
+# for i in b:
+#   print(i)
+
+# for i in a:
+#   print(i)
+
